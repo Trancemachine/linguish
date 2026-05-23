0fbaddfec89495a2d8288750c16fd3ab34abe2be
Trancemachine <2489953060@qq.com>
2026-05-23 19:17:48 +0800

    fix: add tesseract.js to serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // 👇 这行是解决 tesseract.js 在 Vercel 构建问题的关键配置
+  serverExternalPackages: ["tesseract.js"],
 };
 
 export default nextConfig;
